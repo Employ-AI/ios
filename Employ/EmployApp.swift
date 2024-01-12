9db48cae44f59c8907ff81ce0c1069315a534a26
mohsen <ash.wxrz@hotmail.com>
2024-01-12 15:33:26 +0100

    Initial Commit

---
+++ Employ/EmployApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployApp.swift
+//  Employ
+//
+//  Created by Mohsen Rezania on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EmployApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
